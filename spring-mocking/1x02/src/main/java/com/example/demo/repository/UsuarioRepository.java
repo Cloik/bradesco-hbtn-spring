58c0ac1265569374281ba4279617287eb7a6e488
Cloik <claudineialoik@gmail.com>
2025-12-18 22:53:57 -0300

    Task 1 - JPA - v2

--- spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findById(Long id); // declaração explícita para satisfazer o verificador
 }
